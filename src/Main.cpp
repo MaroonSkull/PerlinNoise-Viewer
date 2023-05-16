4fad1a121cdb531edfc999a0dfb3b1d572fef7ea
MaroonSkull <nikobelikkg@gmail.com>
2023-05-16 23:43:34 +0300

    update main and cmake

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,43 @@
 ﻿#include <iostream>
-#include <glad/glad.h>
-#include "GLFW/glfw3.h"
+#include <GL/glew.h>
+#include <glfwpp/glfwpp.h>
 
 
 int main() {
+	auto GLFW = glfw::init(); // RAII GLFW
 	try {
-		//Window MainWindow(1800, 600, "Perlin Noise Generator", GLFWErrorCallback, framebufferSizeCallback);
-		std::cout << "Hello, world!" << std::endl;
+		glfw::WindowHints hints;
+		hints.clientApi = glfw::ClientApi::OpenGl;
+		hints.contextVersionMajor = 4;
+		hints.contextVersionMinor = 6;
+		hints.apply();
+		
+		glfw::Window window{ 1800, 600, "Perlin Noise Generator" };
+		glfw::makeContextCurrent(window);
+
+		int version = gladLoadGL(glfw::getProcAddress);
+		if (version == 0) {
+			printf("Failed to initialize OpenGL context\n");
+			return -1;
+		}
+		printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+
+		while (!window.shouldClose()) {
+			double time = glfw::getTime();
+			glClearColor((sin(time) + 1.0) / 2.0, (cos(time) + 1.0) / 2.0, (-sin(time) + 1.0) / 2.0, 0.0);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			glfw::pollEvents();
+			window.swapBuffers();
+		}
 	}
-	catch (const std::exception& e) {
+	catch (const glfw::Error &e) {
+		std::cerr << "glfwpp exception: " << e.what() << std::endl;
+	}
+	catch (const std::exception &e) {
 		std::cerr << "Standard exception: " << e.what() << std::endl;
 	}
-	catch (const char* e) {
+	catch (const char *e) {
 		std::cerr << e << std::endl;
 	}
 	catch (...) {
@@ -21,19 +47,10 @@ int main() {
 	return 0;
 }
 
-// Обработка любых ошибок GLFW
-void GLFWErrorCallback(int error, const char* description) {
-	std::cerr << stderr << std::endl << "Glfw Error: " << error << ", " << description << std::endl;
-}
-
-// Обработка ресайза окна
-void framebufferSizeCallback(GLFWwindow* window, int32_t width, int32_t height) {
-	glViewport(0, 0, width, height);
-}
-
 // Обработка всех событий ввода: запрос GLFW о нажатии/отпускании клавиш
 // на клавиатуре в данном кадре и соответствующая обработка данных событий
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+	
 }
