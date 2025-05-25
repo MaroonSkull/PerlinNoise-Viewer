b124ae14e4421f87ab66a22f2b4cb9f905ff6c2b
MaroonSkull <33577562+MaroonSkull@users.noreply.github.com>
2025-05-26 00:13:40 +0300

    add implot

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,10 +12,13 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+#include <implot.h>
+
 #include <Perlin.hpp>
 
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 // passing by value is OK
 void handle_eptr(std::exception_ptr eptr) {
@@ -40,6 +43,14 @@ void processInput(glfw::Window &window) {
     window.setShouldClose(true);
 }
 
+// Функция-генератор для ImPlot
+ImPlotPoint Getter(int idx, void* data) {
+    auto* y_data = static_cast<std::vector<float>*>(data);
+    float x = 0 + idx * 1.0;
+    float y = (*y_data)[idx];
+    return ImPlotPoint(x, y);
+}
+
 /**
  * @brief Main function.
  * @details
@@ -59,7 +70,7 @@ int main() {
     hints.contextVersionMinor = 3;
     hints.apply();
 
-    glfw::Window window{256, 200, "Perlin Noise Generator"};
+    glfw::Window window{800, 600, "Perlin Noise Viewer"};
     glfw::makeContextCurrent(window);
 
     int version = gladLoadGL(glfw::getProcAddress);
@@ -70,9 +81,23 @@ int main() {
     std::cerr << "Loaded OpenGL " << GLAD_VERSION_MAJOR(version) << "."
               << GLAD_VERSION_MINOR(version) << std::endl;
 
-    while (!window.shouldClose()) {
+    ImGui::CreateContext();
+    ImGui_ImplOpenGL3_Init(); // #define IMGUI_IMPL_OPENGL_LOADER_GLAD
+    ImGui_ImplGlfw_InitForOpenGL(window, true); // #define IMGUI_IMPL_GLFWD_API
+    ImPlot::CreateContext();
 
-      auto perlin = PerlinFactory<float>::createPerlin(1, 8, 3, 0, 2, 2);
+    while (!window.shouldClose()) {
+      // Clear buffers
+      glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+      auto perlin = PimplPerlinFactory::createPerlin();
+      perlin->numberOfDimensions_ = 1;
+      perlin->pointsBetweenGradients_ = 8;
+      perlin->numberOfGradients_ = 3;
+      perlin->numberOfOctaves_ = 0;
+      perlin->lacunarity_ = 2;
+      perlin->persistence_ = 2;
       auto noise = perlin->getNoise();
 
       std::cout << "Noise values: ";
@@ -81,17 +106,44 @@ int main() {
       }
       std::cout << std::endl;
 
-      auto time{glfw::getTime()};
-      auto alpha{1.};
-      glClearColor((sin(alpha * time) + 1.0) / 2.0,
-                   (cos(alpha * time) + 1.0) / 2.0,
-                   (-sin(alpha * time) + 1.0) / 2.0, 0.0);
-      glClear(GL_COLOR_BUFFER_BIT);
+      // ImGui::Begin("My Window");
+      // if (ImPlot::BeginPlot("My Plot")) {
+      //   ImPlot::PlotLine("My Line Plot", x_data, y_data, 1000);
+      //   ImPlot::EndPlot();
+      // }
+      // ImGui::End();
+
+      // Start ImGui frame
+      ImGui_ImplOpenGL3_NewFrame();
+      ImGui_ImplGlfw_NewFrame();
+      ImGui::NewFrame();
+
+      // Get current window size
+      auto [width, height] = window.getSize();
+
+      // Set ImGui window to 90% of main window size
+      ImGui::SetNextWindowSize(ImVec2(width, height), ImGuiCond_Always);
+
+      // Create a window with a plot
+      if (ImGui::Begin("Plot", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove)) {
+        // Begin a plot
+        if (ImPlot::BeginPlot("My Plot")) {
+          ImPlot::PlotLineG("Line", Getter, &noise, noise.size());
+          ImPlot::EndPlot();
+        }
+      }
+      ImGui::End();
+
+      // Render ImGui + ImPlot
+      ImGui::Render();
+      ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
       processInput(window);
       glfw::pollEvents();
       window.swapBuffers();
     }
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
   } catch (...) {
     eptr = std::current_exception();
   }
